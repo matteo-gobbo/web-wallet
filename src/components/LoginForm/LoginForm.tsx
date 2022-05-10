ab1081d591f8ce0d0ee403208314c75af94fd906
Matteo Gobbo <info@matteogobbo.it>
2022-05-11 00:27:59 +0200

    style: login container

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -25,34 +25,42 @@ const LoginForm: React.FC<Props> = () => {
       }}
     >
       {({ errors, touched }) => {
-        console.log(errors);
         return (
-          <Form>
-            <div>
-              <label htmlFor="username">Username</label>
-              <Field
-                id="username"
-                name="username"
-                placeholder="Mario Rossi"
-                validate={validateUsername}
-              />
-              {errors.username && touched.username && (
-                <div>{errors.username}</div>
-              )}
+          <Form className="mt-8 space-y-8 shadow-lg p-16">
+            <div className="rounded-md shadow-sm">
+              <div>
+                <label htmlFor="username">Username</label>
+                <Field
+                  id="username"
+                  name="username"
+                  validate={validateUsername}
+                  className="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                />
+                {errors.username && touched.username && (
+                  <div className="text-red-500">{errors.username}</div>
+                )}
+              </div>
+              <div className="mt-4">
+                <label htmlFor="password">Password</label>
+                <Field
+                  id="password"
+                  name="password"
+                  validate={validatePassword}
+                  className="rounded relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                />
+                {errors.password && touched.password && (
+                  <div className="text-red-500">{errors.password}</div>
+                )}
+              </div>
             </div>
             <div>
-              <label htmlFor="password">Password</label>
-              <Field
-                id="password"
-                name="password"
-                placeholder="password"
-                validate={validatePassword}
-              />
-              {errors.password && touched.password && (
-                <div>{errors.password}</div>
-              )}
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                Submit
+              </button>
             </div>
-            <button type="submit">Submit</button>
           </Form>
         );
       }}
--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -4,9 +4,21 @@ interface Props {}
 
 const Login: React.FC<Props> = () => {
   return (
-    <>
-      <LoginForm />
-    </>
+    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt="Workflow"
+          />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+        <LoginForm />
+      </div>
+    </div>
   );
 };
 
